3fac5e0d3fe2d91a9825111b47c6629388f650b9
Martynas <packmaster@riawolf.com>
2018-12-05 13:43:52 +0200

    su komentarais

--- tema2/task3.php
+++ tema2/task3.php
@@ -14,28 +14,32 @@ include("../fragments/menu2.php");
     puslapis parodo visus įvestus žodžius.</p>
 <form action="#" method="get">
     <input type="text" name="zodis"><br>
-    <input type="checkbox" name="clean">Valyti sesija <br>
+    <input type="checkbox" name="clean">Valyti sesija<br>
     <input type="submit">
 </form>
 <?php
-
+// sesijos kintamajam "sakinys" duodama pradine reiksme
 if (!isset($_SESSION["sakinys"])) {
     $_SESSION["sakinys"] = "";
 }
 if (isset($_REQUEST["zodis"])) {
     echo "<atsakymas>";
-
+    // pasiimam zodi is request
     $zodis = $_REQUEST["zodis"];
-    // va cia sudeti logika
     if ($zodis != "pabaiga") {
+        // jeigu zodis ne pabaiga, prideti prie sesijos kintamojo zodi ir ji parodyti
         $_SESSION["sakinys"] .= "$zodis ";
         echo $zodis;
     } else {
+        // jeigu zodis pabaiga, parodyti kas saugoma sesijos sakinyje
         echo $_SESSION["sakinys"];
     }
 
     echo "</atsakymas>";
 }
+// sesijos isvalymo galimybe
+// jeigu buvo uzdeta varnele
+// sesijos sakini padaryti tusciu
 if (isset($_REQUEST["clean"]) && $_REQUEST["clean"] == "on") {
     $_SESSION["sakinys"] = "";
 }
