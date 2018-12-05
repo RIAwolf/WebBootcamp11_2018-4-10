daa4da1f21282af1bff5ac3ffb741db4f0734d5f
Martynas <packmaster@riawolf.com>
2018-12-05 14:53:16 +0200

    uzduotis 7 su komentarais

--- tema2/task7.php
+++ tema2/task7.php
@@ -9,24 +9,35 @@
 <?php
 include("../fragments/menu2.php");
 ?>
-<p>Sukurti puslapį, kuris leistų įvesti skaičius atskirtus kableliais. Paspaudus submit, turi būti atvaizduojama įvestų skaičių suma ir visi įvesti skaičiai.</p>
+<p>Sukurti puslapį, kuris leistų įvesti skaičius atskirtus kableliais. Paspaudus submit, turi būti atvaizduojama įvestų
+    skaičių suma ir visi įvesti skaičiai.</p>
 <form action="#" method="get">
-    <input type="text" name="zodziai">
+    <input type="text" name="skaiciai">
     <input type="submit">
 </form>
 <?php
+// susikurti sumos kintamaji ir priskirti pradzioje 0;
+$suma = 0;
 
-if (isset($_REQUEST["zodziai"])) {
+// patikrinti ar request viduje yra kintamasis skaiciai
+if (isset($_REQUEST["skaiciai"])) {
     echo "<atsakymas>";
-    $zodziai = $_REQUEST["zodziai"];
-    $masyvas = explode(",", $zodziai);
-
-    foreach ($masyvas as $kaliosikas69) {
-        echo "$kaliosikas69 <br>";
+    // jeigu yra kintamasis skaiciai, panaudoti bomba kad gauti skaiciu masyva
+    $masyvas = explode(",", $_REQUEST["skaiciai"]);
+    // pereiti per masyva ir imti elementa po viena
+    foreach ($masyvas as $elementas) {
+        // paemus masyvo elementa paversi ji skaicium ir pasideti i kintamaji sk
+        $sk = floatval($elementas);
+        // sk prideti prie sumos
+        $suma += $sk;
+        // atvaizduoti sk
+        echo "$sk <br>";
     }
-
+    // po ciklo atvaizduoti suma
+    echo "$suma";
     echo "</atsakymas>";
 }
+
 ?>
 </body>
 </html>
