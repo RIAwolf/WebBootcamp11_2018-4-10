a56d35822e5cddb15fc70bce3477de1343c507ba
Martynas <packmaster@riawolf.com>
2018-12-05 13:50:46 +0200

    4tas su komentarais

--- fragments/menu2.php
+++ fragments/menu2.php
@@ -3,4 +3,5 @@
     <li><a href="/tema2/task1.php">Užduotis 1</a></li>
     <li><a href="/tema2/task2.php">Užduotis 2</a></li>
     <li><a href="/tema2/task3.php">Užduotis 3</a></li>
+    <li><a href="/tema2/task4.php">Užduotis 4</a></li>
 </ul>
