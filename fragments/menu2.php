e84151eb6e281c01a99efcd16d26e26245ebadb9
Martynas <packmaster@riawolf.com>
2018-12-05 15:04:03 +0200

    uzduotis 8 su komentarais

--- fragments/menu2.php
+++ fragments/menu2.php
@@ -6,5 +6,6 @@
     <li><a href="/tema2/task4.php">Užduotis 4</a></li>
     <li><a href="/tema2/task5.php">Užduotis 5</a></li>
     <li><a href="/tema2/task6.php">Užduotis 6</a></li>
-    <li><a href="/tema2/task7.php">Užduotis 6</a></li>
+    <li><a href="/tema2/task7.php">Užduotis 7</a></li>
+    <li><a href="/tema2/task8.php">Užduotis 8</a></li>
 </ul>
--- tema2/task8.php
+++ tema2/task8.php
@@ -9,15 +9,15 @@
 <?php
 include("../fragments/menu2.php");
 ?>
-<p>Sukurti puslapį, kuris leistų įvesti skaičius atskirtus kableliais. Paspaudus submit, turi būti atvaizduojama įvestų
-    skaičių suma ir visi įvesti skaičiai.</p>
+<p>Sukurti puslapį, kuris leistų įvesti skaičius atskirtus kableliais. Paspaudus submit, atvaizduoti tik tuos skaičius
+    kurie buvo didesni nei 100.</p>
 <form action="#" method="get">
     <input type="text" name="skaiciai">
     <input type="submit">
 </form>
 <?php
 // susikurti sumos kintamaji ir priskirti pradzioje 0;
-$suma = 0;
+
 // patikrinti ar request viduje yra kintamasis skaiciai
 if (isset($_REQUEST["skaiciai"])) {
     echo "<atsakymas>";
@@ -27,13 +27,12 @@ if (isset($_REQUEST["skaiciai"])) {
     foreach ($masyvas as $elementas) {
         // paemus masyvo elementa paversi ji skaicium ir pasideti i kintamaji sk
         $sk = floatval($elementas);
-        // sk prideti prie sumos
-        $suma += $sk;
-        // atvaizduoti sk
-        echo "$sk <br>";
+        // ar sk didesnis uz 100
+        if ($sk > 100) {
+            // atvaizduoti sk
+            echo "$sk <br>";
+        }
     }
-    // po ciklo atvaizduoti suma
-    echo "$suma";
     echo "</atsakymas>";
 }
 ?>
