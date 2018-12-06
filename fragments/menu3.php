0b9589520632fc45aba885b3f376b0d5b0202524
Martynas <packmaster@riawolf.com>
2018-12-06 10:40:39 +0200

    oop task 2

--- fragments/menu3.php
+++ fragments/menu3.php
@@ -1,4 +1,5 @@
 <ul>
     <li><a href="/tema3/">Pradžia</a></li>
     <li><a href="/tema3/task1.php?name=Petras">Užduotis 1</a></li>
+    <li><a href="/tema3/task2.php?zodis=labas">Užduotis 2</a></li>
 </ul>
